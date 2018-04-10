14385df7baf942fab067af7a1998b91a42660943
Sergio Sepulveda <sergio.sepulveda.gonzalez@gmail.com>
2018-04-10 18:23:29 -0300

    Theme tuclase - Formato TCTP para emails de autoregistro e invitaciones

--- CHANGELOG-TCTP.md
+++ CHANGELOG-TCTP.md
@@ -1,6 +1,10 @@
 Theme HumHub Opensource TuClase
 ===============================
 
-Este archivo registra las personalizaciones al theme tuclase (base de otros theme):
+Este archivo registra las personalizaciones al theme tuclase (base de otros themes):
 
 - Se modifica layout de la página de inicio incorporando HTML de bienvenida y form de login.
+- Se modifica el formato de los emails de autoregistro y de invitaciones al sistema y espacios:
+    * Se agrega el logotipo del sistema en la cabecera.
+    * Se elimina el nombre del sistema (aplicación) de la cabecera.
+    * Se mueve link 'Unsuscribe' debajo de la cabecera para facilitar la visibilidad.
---
+++ views/user/mails/UserInviteSpace.php
@@ -0,0 +1,201 @@
+<?php
+
+
+use yii\helpers\Html;
+
+?>
+<tr>
+    <td align="center" valign="top"   class="fix-box">
+
+        <!-- start  container width 600px -->
+        <table width="600"  align="center" border="0" cellspacing="0" cellpadding="0" class="container"  style="background-color: #ffffff; ">
+
+
+            <tr>
+                <td valign="top">
+
+                    <!-- start container width 560px -->
+                    <table width="540"  align="center" border="0" cellspacing="0" cellpadding="0" class="full-width" bgcolor="#ffffff" style="background-color:#ffffff;">
+
+
+                        <!-- start text content -->
+                        <tr>
+                            <td valign="top">
+                                <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" >
+                                    <tr>
+                                        <td valign="top" width="auto" align="center">
+                                            <!-- start button -->
+                                            <table border="0" align="center" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td width="auto"  align="center" valign="middle" height="28" style=" background-color:#ffffff; background-clip: padding-box; font-size:26px; font-family:Open Sans, Arial,Tahoma, Helvetica, sans-serif; text-align:center;  color:#a3a2a2; font-weight: 300; padding-left:18px; padding-right:18px; ">
+
+                                                        <span style="color: #555555; font-weight: 300;">
+                                                            <?php echo Yii::t('UserModule.views_mails_UserInviteSpace', 'Tienes una invitación para unirte a {space} en {name}.', ['space' => '<strong>' . Html::encode($space->name) . '</strong>', 'name' => Html::encode(Yii::$app->name)]); ?>
+                                                        </span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            <!-- end button -->
+                                        </td>
+                                    </tr>
+
+
+
+                                </table>
+                            </td>
+                        </tr>
+                        <!-- end text content -->
+
+
+                    </table>
+                    <!-- end  container width 560px -->
+                </td>
+            </tr>
+        </table>
+        <!-- end  container width 600px -->
+    </td>
+</tr>
+
+<tr>
+    <td align="center" valign="top" class="fix-box">
+
+        <!-- start  container width 600px -->
+        <table width="600" align="center" border="0" cellspacing="0" cellpadding="0" class="container" bgcolor="#ffffff"
+               style="background-color: #ffffff; border-bottom-left-radius: 4px; border-bottom-left-radius: 4px;">
+            <tr>
+                <td valign="top">
+
+                    <!-- start container width 560px -->
+                    <table width="560" align="center" border="0" cellspacing="0" cellpadding="0" class="full-width"
+                           bgcolor="#ffffff" style="background-color:#ffffff;">
+
+                        <!-- start image and content -->
+                        <tr>
+                            <td valign="top" width="100%">
+
+                                <!-- start content left -->
+                                <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+
+                                    <!--start space height -->
+                                    <tr>
+                                        <td height="20"></td>
+                                    </tr>
+                                    <!--end space height -->
+
+
+
+                        <!-- end  content top-->
+
+
+                        <!--start space height -->
+                        <tr>
+                            <td height="5" class="col-underline"></td>
+                        </tr>
+                        <!--end space height -->
+
+
+                    </table>
+                    <!-- end content left -->
+
+
+                </td>
+            </tr>
+            <!-- end image and content -->
+
+        </table>
+        <!-- end  container width 560px -->
+    </td>
+</tr>
+</table>
+<!-- end  container width 600px -->
+</td>
+</tr>
+
+<tr>
+    <td align="center" valign="top"   class="fix-box">
+
+        <!-- start  container width 600px -->
+        <table width="600"  align="center" border="0" cellspacing="0" cellpadding="0" class="container"  style="background-color: #ffffff; ">
+
+
+            <tr>
+                <td valign="top">
+
+                    <!-- start container width 560px -->
+                    <table width="540"  align="center" border="0" cellspacing="0" cellpadding="0" class="full-width" bgcolor="#ffffff" style="background-color:#ffffff;">
+
+
+                        <!-- start text content -->
+                        <tr>
+                            <td valign="top">
+                                <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" >
+
+
+                                    <!-- start text content -->
+                                    <tr>
+                                        <td valign="top">
+                                            <table border="0" cellspacing="0" cellpadding="0" align="center" >
+
+
+                                                <!--start space height -->
+                                                <tr>
+                                                    <td height="15" ></td>
+                                                </tr>
+                                                <!--end space height -->
+
+                                                <tr>
+                                                    <td  style="font-size: 14px; line-height: 22px; padding-left: 50px; padding-right: 50px; font-family:Open Sans,Arial,Tahoma, Helvetica, sans-serif; color:#777777; font-weight:300; text-align:center; ">                                                        
+                                                        <!-- START: CONTENT -->
+                                                        <?php echo Yii::t('UserModule.views_mails_UserInviteSpace', 'En {name} podrás compartir contenido educativo, experiencias pedagógicas y nuevas metodologías de enseñanza, además de ejercer el aprendizaje colaborativo y generar nuevas redes profesionales.', ['name' => '<strong>' . Html::encode(Yii::$app->name) . '</strong>']); ?><br/><br/>
+                                                        <?php echo Yii::t('UserModule.views_mails_UserInviteSpace', '¡Te esperamos!'); ?><br/>
+                                                        &nbsp;
+                                                        <!-- END: CONTENT -->
+                                                    </td>
+                                                </tr>
+
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <!-- end text content -->
+
+                                    <tr>
+                                        <td valign="top" width="auto" align="center">
+                                            <!-- start button -->
+                                            <table border="0" align="center" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td width="auto"  align="center" valign="middle" height="32" style=" background-color:<?= Yii::$app->view->theme->variable('primary') ?>;  border-radius:5px; background-clip: padding-box;font-size:14px; font-family:Open Sans, Arial,Tahoma, Helvetica, sans-serif; text-align:center;  color:#ffffff; font-weight: 600; padding-left:30px; padding-right:30px; padding-top: 5px; padding-bottom: 5px;">
+
+                                                        <span style="color: #ffffff; font-weight: 300;">
+                                                            <a href="<?php echo $registrationUrl; ?>" style="text-decoration: none; color: #ffffff; font-weight: 300;">
+                                                                <strong><?php echo Yii::t('UserModule.views_mails_UserInviteSpace', 'Sign up now'); ?></strong>
+                                                            </a>
+                                                        </span>
+                                                    </td>
+
+                                                </tr>
+                                            </table>
+                                            <!-- end button -->
+                                        </td>
+
+                                    </tr>
+
+                                </table>
+                            </td>
+                        </tr>
+                        <!-- end text content -->
+
+                        <!--start space height -->
+                        <tr>
+                            <td height="20" ></td>
+                        </tr>
+                        <!--end space height -->
+
+
+                    </table>
+                    <!-- end  container width 560px -->
+                </td>
+            </tr>
+        </table>
+        <!-- end  container width 600px -->
+    </td>
+</tr>
